6972d4747afae0170c4ab4947b7efad689272736
AldoJh <108147181+AldoJh@users.noreply.github.com>
2022-06-25 23:47:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Training Center</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar bg-light fixed-top">
+  <div class="container-fluid">
+<img src="images/logo.png" width="50" height="50" class="d-inline-block align-text-top">
+  <h5><a class="navbar-brand" text-align="center" href="">Training center</a></h5>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Training Center</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">Registrasi</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link active" href="">Pengumuman</a>
+          </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<br>
+<br>
+<br>
+
+<div class="card mb-3" style="max-width: 1400px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="images/logo.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h4 class="card-title">Profile Aceh Oil Institute</h4>
+        <p class="card-text">Aceh Oil Institute is a private training provider in the oil and gas sector. Providing the best services to the
+consumers through high-quality training courses is our main obligation. Our foundation was established
+in 2012. We began to offer the training courses concept to the government and industry in 2014. Our
+primary training courses focus on providing technical training (theoretical and practical) and internship
+program in the oil and gas industry sector.
+By combining the Integrated Training Model with Employer-Based Training (ITM&EBT), and building on
+the success of training courses in the oil and gas sector. Aceh Oil Institute is, today, the first public training
+provider as the partner of the Department of Manpower in Aceh that declared successful in educating,
+training and promoting graduates to work in the oil and gas industry.
+We put forward the principle of responsible service to our customers, maintaining the highest standards
+in training courses and programs. The success of our trainees to fill-in in required positions, and having
+the responsibility of the profession with satisfactory work results by showing a good attitude is our the
+top objective.
+In addition boosting competency, we also take a part in promoting graduates to the local company or
+employers, professional manpower suppliers, the oil and gas job market, and to our connections in the
+energy company. </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<br>
+<br>
+
+<h1>Program Unggulan</h1>
+<div class="row">
+ 
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/1.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href="detail.php"><h4>Oil and Gas Production Operator</h4></a></div>
+       This oil and gas operations resource focuses on world oil production, offshore oil production, oil management initiatives, and gas production.
+      </div>
+    </div>
+  </div>
+
+<div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/2.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Industrial Safety in Oil and Gas Industries </h4></a></div>
+       Workers in the oil and gas industry continue to be one of the groups at the highest risk of injuries and fatalities on the job compared to all other U.S. industries.
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/3.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Mechanical Maintenance and Rotating Equipment</h4></a></div>
+        (Mechanical maintenance-Rotating/Rotating equipment’s)ready for day to day mechanical maintenance job and for interview purpose      
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/4.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Instrumentation Technician </h4></a></div>
+        Instrumentation technicians are professionals who specialize in installing and repairing machines and tools. They perform an important role in helping industrial plants process materials or develop products for consumers.
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/5.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Electrician </h4></a></div>
+        An electrician is a tradesperson specializing in electrical wiring of buildings, transmission lines, stationary machines, and related equipment.
+        <br>
+        <br>
+        <br>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/6.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Non-Destructive Testing Training</h4></a></div>
+        Non-destructive testing (NDT) training from the SGS NDT Training Center – comprehensive NDT training, examinations and certification for your personnel in accordance with European, international and American standards
+        <br>
+        <br>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/7.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>H2S Safety Awareness Training</h4></a></div>
+        Scope This course is designed for personnel who is working in oil, gas,geothermal & petrochemical or industry with poisonous gases area. 
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/8.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Firefighting Training</h4></a></div>
+       To provide community fire fighters with the skills and knowledge required to adequately perform their respective roles and responsibilities within the fire service.
+       <br>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/9.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4> Process Safety Management</h4></a></div>
+        A process safety management system is an analytical tool focused on preventing releases of any substance defined as a "highly hazardous chemical" by the EPA or OSHA.
+        <br>
+        <br>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-4 col-sm-12 mb-3">
+    <div class="card">
+      <img src="images/10.jpg" class="card-img-top" alt="..." >
+ 
+      <div class="card-body">
+        <div class="card-title"><a href=""><h4>Nursing</h4></a></div>
+        Nursing is a profession within the health care sector focused on the care of individuals, families, and communities so they may attain, maintain, or recover optimal health and quality of life.
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+
+<h1>Sosial Media</h1>
+                                <div class="about_socail m-top-50">
+                                    <ul class="list-inline">
+                                        <li><span class="divider"></span></li>
+                                        <li><a href=""><i class="fa fa-gmail"></i>gmail</a></li>
+                                        <li><a href=""><i class="fa fa-whatsapp"></i>Whatsap</a></li>
+                                        <li><a href=""><i class="fa fa-linkedin"></i>Linkedin</a></li>
+                                        <li><a href=""><i class="fa fa-instagram"></i>Insagram</a></li>
+                                    </ul>
+                                </div>
+
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
